b56c8fd1088798ead8ca696a3f51141dcfd21878
gottabpro <sj931293@gmail.com>
2021-03-30 12:52:45 +0530

    First commit

---
+++ wallet/transaction-pool.test.js
@@ -0,0 +1,37 @@
+const TrasanctionPool = require('./transaction-pool')
+
+const Transaction = require('./transaction')
+const Wallet = require('./')
+
+describe('TransactionPool', () => {
+  let transactionPool, transaction, senderWallet;
+
+  beforeEach(() => {
+    transactionPool = new TrasanctionPool()
+    senderWallet = new Wallet()
+    transaction = new Transaction({
+      senderWallet: senderWallet,
+      recipient: 'fake-recipient',
+      amount: 50
+    })
+  })
+
+  describe('setTransaction()', () => {
+    it('adds a transaction', () => {
+      transactionPool.setTransaction(transaction)
+
+      expect(transactionPool.transactionMap[transaction.id]).toBe(transaction)
+    })
+  })
+
+  describe('existingTransaction()', () => {
+    it('returns an existing transaction given an input address', () => {
+      transactionPool.setTransaction(transaction)
+
+      expect(
+        transactionPool.existingTransaction({inputAddress: senderWallet.publicKey})
+      ).toBe(transaction)
+      
+    })
+  })
+})
